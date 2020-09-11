805fb96d32491dacbd0f4d8c1915f7476c26686f
Kevin Botero <kbotero201@gmail.com>
2020-09-11 01:34:26 +0000

    Automatically backed up by Learn

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -14,7 +14,7 @@ describe "./cartoon_collections.rb" do
 
   describe "summon_captain_planet" do
     it "takes in an array of strings and returns a new array where each string is capitalized and has an exclamation point" do
-      
+
 
       expect(summon_captain_planet(small_calls)).to eq(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])
       expect(summon_captain_planet(other_calls)).to eq(["Plants!", "Humans!", "Mushrooms!", "Brains!"])
@@ -39,7 +39,7 @@ describe "./cartoon_collections.rb" do
     describe "takes in an array of strings" do
       it "checks for and valid planeteer calls and returns the first found if present" do
         expect(find_valid_calls(["Earth!", "Wind!", "Fire!", "Water!", "Heart!"])).to eq("Earth!")
-        expect(find_valid_calls(["Wood!", "Wind!", "Brass!", "Enamel!", "Heart!"])).to eq("Wind!")
+        expect(find_valid_calls(["Wood!", "Wind!", "Brass!", "Enamel!", "Heart!"])).to eq("Wood!")
       end
 
       it "if no valid planeteer calls are found, returns nil" do
@@ -50,7 +50,7 @@ describe "./cartoon_collections.rb" do
   end
 end
 
-#   
+#
 # end
 
 # describe "takes in an array of strings and" do
@@ -67,4 +67,3 @@ end
 #     expect(long_planeteer_calls(small_calls)).to eq(false)
 #   end
 # end
-
